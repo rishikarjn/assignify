3f146b57f8c6dfada71aefcc906c0bcb411bd6cc
rishikarjn <rishikasingh2109@gmail.com>
2025-12-30 11:56:48 +0530

    Employee Card setup

--- assignify/src/Employess/Employess.jsx
+++ assignify/src/Employess/Employess.jsx
@@ -7,8 +7,16 @@ const Employess = () => {
     <div>
       <ul className="list bg-base-100 rounded-box shadow-md">
   
-   
-  <li className="list-row">
+   <EmployeeCard/>
+  </ul>
+    </div>
+    </Layout>
+  )
+}
+
+const EmployeeCard=()=>{
+  return (
+    <li className="list-row">
     <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp"/></div>
     <div>
       <div>Dio Lupa</div>
@@ -24,11 +32,6 @@ const Employess = () => {
       <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
     </button>
   </li>
-  
- 
-</ul>
-    </div>
-    </Layout>
   )
 }
 
--- assignify/src/navbar/Navbar.jsx
+++ assignify/src/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-
 import Layout from '../layout/Layout'
+import { FaPlus } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -13,13 +13,12 @@ const Navbar = () => {
   </div>
   <div className="navbar-end">
     <button className="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
-    </button>
+      <FaPlus/>
+  </button>
     <button className="btn btn-ghost btn-circle">
       <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-        <span className="badge badge-xs badge-primary indicator-item"></span>
-      </div>
+      <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+       </div>
     </button>
   </div>
 </div>
