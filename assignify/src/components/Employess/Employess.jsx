bf47d03d7f010e7eb0ae8ceabc11a3ffd6027563
rishikarjn <rishikasingh2109@gmail.com>
2026-01-03 23:59:24 +0530

    Integrating highlight features

--- assignify/src/components/Employess/Employess.jsx
+++ assignify/src/components/Employess/Employess.jsx
@@ -15,7 +15,7 @@ const Employess = () => {
     <Layout>
     <div>
       <ul className="list bg-base-100 rounded-box shadow-md">
-  
+  {employees.length===0 && <p>No Employees Found</p>}
   {employees.map((details)=>(
       <EmployeeCard key={details.id} details={details}/>
   ))}
@@ -30,6 +30,18 @@ const Employess = () => {
 const EmployeeCard=({details})=>{
 
   const dispatch=useDispatch();
+
+  const handleHighlight=(details)=>{
+    dispatch(updateEmployees({
+       id:details.id,
+      details:{
+        ...details,
+        highlight:true
+  }
+
+    }))
+  }
+   
   return (
     <li className="list-row">
     <div><img className="size-10 rounded-box" src={details.profileUrl}/></div>
@@ -47,7 +59,8 @@ const EmployeeCard=({details})=>{
       className="btn btn-square btn-ghost">
       <MdOutlineDeleteOutline className='text-xl'/>
     </button>
-    <button onClick={()=>dispatch(updateEmployees)} className="btn btn-square btn-ghost">
+    <button onClick={()=>handleHighlight(details)} className="btn btn-square btn-ghost">
+      
       <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
     </button>
   </li>
